45115815d37ed24449b3916ebded328020502402
Owen Tidy <owen.tidy@ballys.com>
2026-01-14 13:50:08 +0000

    enable RGB matrix support and remove better_led.c from the keymap

--- keyboards/doio/kb16/rev2/keymaps/vial_clownfish/rules.mk
+++ keyboards/doio/kb16/rev2/keymaps/vial_clownfish/rules.mk
@@ -4,8 +4,10 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 
+RGB_MATRIX_ENABLE = yes
+
 LTO_ENABLE = no
 SRC += oled_layers.c
-SRC += better_led.c
+SRC += board.c
 
 USER_NAME := clownfish
